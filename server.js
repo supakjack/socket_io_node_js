869c71e6de629991cf266dfb687864e2fbd36be1
supakjack <48548611+supakjack@users.noreply.github.com>
2019-11-03 11:41:58 +0700

    full
    full

---
+++ server.js
@@ -0,0 +1,21 @@
+var app = require('express')()
+var http = require('http').Server(app)
+var io = require('socket.io')(http)
+
+app.get('/', (req, res) => {
+    // res.end('Jack Mobile');
+    res.sendFile(__dirname + '/index.html')
+})
+
+io.on('connection', (socket) => {
+    console.log("a ./ client connected")
+    socket.on('chat message', (msg) => {
+        console.log('message : ' + msg)
+        // Reply
+        io.emit('chat message', msg)
+    })
+})
+
+http.listen(3000, () => {
+    console.log('Listening on *:3000')
+})
